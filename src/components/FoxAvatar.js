465a0410f465f5cdd2fd7adb56a76214021ed3d3
Your GitHub Username <your.email@example.com>
2025-02-02 21:33:52 +0300

    Add GitHub Pages deployment workflow

--- src/components/FoxAvatar.js
+++ src/components/FoxAvatar.js
@@ -3,22 +3,14 @@ import './FoxAvatar.css';
 
 const FoxAvatar = ({ isSpeaking, isListening }) => {
   return (
-    <div className={`fox-container ${isListening ? 'listening' : ''} ${isSpeaking ? 'talking' : ''}`}>
-      <div className="fox-wrapper">
-        <div className={`fox ${isSpeaking ? 'talking' : ''}`}>
-          <div className="fox__head">
-            <div className="fox__face">
-              <div className="fox__eye fox__eye--left"></div>
-              <div className="fox__eye fox__eye--right"></div>
-              <div className="fox__nose"></div>
-              <div className="fox__mouth"></div>
-            </div>
-            <div className="fox__ear fox__ear--left"></div>
-            <div className="fox__ear fox__ear--right"></div>
-            <div className="fox__side fox__side--left"></div>
-            <div className="fox__side fox__side--right"></div>
-          </div>
-        </div>
+    <div className={`fox-container ${isListening ? 'listening' : ''}`}>
+      <div className={`fox ${isSpeaking ? 'talking' : ''}`}>
+        <div className="ear left"></div>
+        <div className="ear right"></div>
+        <div className="eye left"></div>
+        <div className="eye right"></div>
+        <div className="nose"></div>
+        <div className="mouth"></div>
       </div>
     </div>
   );
