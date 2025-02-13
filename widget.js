7b64a0b1b6e8b7f80be554249dc3eea86af1515b
ABEDQWASMI <ABEDQWASMI@users.noreply.github.com>
2025-02-13 20:50:30 +0000

    deploy: f91863c306911112d6f9f40c423148bd4cecffbe

--- widget.js
+++ widget.js
@@ -18,6 +18,29 @@
     document.body.appendChild(widget);
     console.log('Container created');
 
+<<<<<<< HEAD
+    // Add media preload
+    var mediaFiles = [
+        'chat.9852bbd49db43ab5ba6c39258c8e5f42.svg',
+        'circleDiv.cf0e90a4f080ecaf1bc910140b0576bd.svg',
+        'Closed.0a7e1bfe83eb8fcad1b4ab33ff35a996.svg',
+        'game.64a20cb10b927a4036c3baaed638c130.svg',
+        'logo.8562c214b741e5bde7af390722764cda.svg',
+        'mic.cc6bbaec2dbe3cac78dcaca5160d0a7a.svg',
+        'show.044c5aec5ea62977acb394b98fa3bbf9.svg',
+        'start.ac15180422b613275ca6853eb504839a.svg'
+    ];
+
+    mediaFiles.forEach(function(file) {
+        var link = document.createElement('link');
+        link.rel = 'preload';
+        link.as = 'image';
+        link.href = 'https://leapthelimit.github.io/finlit-widget/static/media/' + file;
+        document.head.appendChild(link);
+    });
+
+=======
+>>>>>>> f7b53b8d7a043b023e3209e28c2d1f05363d58c6
     // Add CSS with error checking
     var styles = document.createElement('link');
     styles.rel = 'stylesheet';
