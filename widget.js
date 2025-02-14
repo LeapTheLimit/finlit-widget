9d6753c610a37b4b67abea89e1c7a9c293c221ec
Your GitHub Username <your.email@example.com>
2025-02-14 17:48:24 +0300

    Add GitHub Pages deployment workflow

--- widget.js
+++ widget.js
@@ -19,20 +19,12 @@
     // Load required styles
     const styles = document.createElement('link');
     styles.rel = 'stylesheet';
-<<<<<<< HEAD
-    styles.href = './css/main.479e324a.css'; // Updated path
-=======
-    styles.href = '/finlit-widget/static/css/main.479e324a.css'; // Updated path
->>>>>>> 1266a530d4f1f4b9388b7d17c34ea5c57988a8b6
+    styles.href = './css/main.479e324a.css'; // Ensure this path is correct
     
     styles.onload = () => {
         console.log('CSS loaded successfully.');
         // Load required scripts
-<<<<<<< HEAD
-        loadScript('./js/main.3f31f8e3.js'); // Updated path
-=======
-        loadScript('/finlit-widget/static/js/main.3f31f8e3.js'); // Updated path
->>>>>>> 1266a530d4f1f4b9388b7d17c34ea5c57988a8b6
+        loadScript('./js/main.3f31f8e3.js'); // Ensure this path is correct
     };
 
     styles.onerror = () => {
